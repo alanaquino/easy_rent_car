83fa4ed39c615f8439880052d0806a126fff7c97
JhonRenzox77 <joangil077@gmail.com>
2023-03-24 10:44:20 -0400

    cambio
    texto agregado

--- reservar_auto.php
+++ reservar_auto.php
@@ -1,17 +1,20 @@
 <?php include('head.php'); ?>
 
+<?php include('./header.php'); ?>
+
+
 <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="css/style.css" rel="stylesheet">
+  </head>
+  <body>
+  
+<p> hola mundo </p>
 
-  <html lang="es">
 
-<body>
-
-<p>hola renzo</p>
-
-
-</body>
-
-
-
-
-   </html>
+  </body>
+</html>
