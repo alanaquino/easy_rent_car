3af1da64b724121b3b107c80a6ad6d20897c0903
JhonRenzox77 <joangil077@gmail.com>
2023-05-06 16:19:45 -0400

    editar algunas informaciones de los vehiculos

--- admin/config/db.php
+++ admin/config/db.php
@@ -8,9 +8,10 @@ ob_start();
 //    session_start();
 //}
 
-$hostname = "50.87.151.191";
-$username = "rotary40_rental";
-$password = "HG&]170jsh]l";
+//$hostname = "50.87.151.191";
+$hostname = "localhost";
+$username = "root";
+$password = "";
 $dbname = "rotary40_car_rental_app";
 
 $connection = mysqli_connect($hostname, $username, $password, $dbname) or die("Database connection not established.");
---
+++ admin/footer.php
@@ -0,0 +1,57 @@
+		<!-- footer_section - start
+		================================================== -->
+		<footer class="footer_section clearfix">
+			<div class="footer_widget_area sec_ptb_100 clearfix" data-bg-color="#F2F2F2">
+				<div class="container">
+					<div class="row justify-content-center">
+					
+						<div class="col-md-6 col-sm-12">
+							<div class="footer_about text-center" data-aos="fade-up" data-aos-delay="100">
+							
+								<div class="form_item mb-15">
+									<form action="#">
+										<input id="footer_search" type="search" name="search" placeholder="Search">
+										<label for="footer_search" class="input_icon"><i class="fal fa-search"></i></label>
+									</form>
+								</div>
+							
+								<div class="brand_logo mb_30">
+									<a href="index.html">
+										<img src="assets/images/logo/logo_02_1x.png" srcset="assets/images/logo/logo_02_2x.png 2x" alt="logo_not_found">
+									</a>
+								</div>
+								<p class="mb_15">
+									Cras sit amet mi non orci pretium consectetur. Donec iaculis ante ac sollicitudin luctus. Phasellus ut lacus lacus. Phasellus sagittis ex id tortor tincidunt luctus. Donec consectetur consequat bibendum
+								</p>
+								
+							</div>
+						</div>
+
+						
+
+						
+					</div>
+				</div>
+			</div>
+
+			<div class="footer_bottom text-white clearfix" data-bg-color="#000C21">
+				<div class="container">
+					<div class="row align-items-center justify-content-lg-between">
+						<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+							<p class="copyright_text mb-0">Copyright © 2023. Easy Rent by <a class="author_links text-white" href="#!">Grupo 2</a></p>
+						</div>
+
+						<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
+							<ul class="primary_social_links ul_li_right clearfix">
+								<li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
+								<li><a href="#!"><i class="fab fa-instagram"></i></a></li>
+								<li><a href="#!"><i class="fab fa-twitter"></i></a></li>
+								<li><a href="#!"><i class="fab fa-youtube"></i></a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- footer_section - end
+		================================================== -->
